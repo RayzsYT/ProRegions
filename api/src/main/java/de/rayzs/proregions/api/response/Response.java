46b06e013522d9f094ab61c61affa15fddaaffc4
rayzsyt <alihan290404@gmail.com>
2026-02-23 21:46:55 +0100

    Moved send method to the top

--- api/src/main/java/de/rayzs/proregions/api/response/Response.java
+++ api/src/main/java/de/rayzs/proregions/api/response/Response.java
@@ -3,18 +3,23 @@ package de.rayzs.proregions.api.response;
 import org.bukkit.Sound;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
+import org.jspecify.annotations.Nullable;
 
 public interface Response extends ConfigurationSerializable {
 
-    void setChatMessage(final String text);
-
-    void setActionbarMessage(final String text);
-
-    void setTitle(final String text);
-
-    void setSubtitle(final String text);
-
-    void setSound(final Sound sound, final double volume, final double pitch);
-
     void send(final Player player);
+
+    void setChatMessage(final @Nullable String text);
+
+    void setActionbarMessage(final @Nullable String text);
+
+    void setTitle(final @Nullable String text);
+
+    void setSubtitle(final @Nullable String text);
+
+    void setSound(
+            final @Nullable Sound sound,
+            final double volume,
+            final double pitch
+    );
 }
