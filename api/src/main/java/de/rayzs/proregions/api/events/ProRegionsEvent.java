180e9220998d72561d8d9b78b73f96ef49882a45
rayzsyt <alihan290404@gmail.com>
2026-02-27 03:42:34 +0100

    Added

---
+++ api/src/main/java/de/rayzs/proregions/api/events/ProRegionsEvent.java
@@ -0,0 +1,41 @@
+package de.rayzs.proregions.api.events;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class ProRegionsEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    protected boolean cancelled = false;
+    protected boolean response = false;
+
+    public void setSendResponse(boolean response) {
+        this.response = response;
+    }
+
+    public boolean canSendResponse() {
+        return this.response;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(final boolean bool) {
+        this.cancelled = bool;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
---
+++ api/src/main/java/de/rayzs/proregions/api/events/RegionLeaveEvent.java
@@ -0,0 +1,41 @@
+package de.rayzs.proregions.api.events;
+
+import de.rayzs.proregions.api.region.Region;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class RegionLeaveEvent extends ProRegionsEvent {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+    private final Region region;
+
+    public RegionLeaveEvent(
+            final Player player,
+            final Region region,
+            boolean cancelled
+    ) {
+        this.player = player;
+        this.region = region;
+        this.cancelled = cancelled;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
